1ae2365a8df912db774e580c5086f23c756b221b
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-04-18 17:50:26 -0400

    simple html through node

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage('tom', 'tomhub'), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
